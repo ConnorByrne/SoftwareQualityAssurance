8c46c4c17480bb3c41269345433726c1cdef6435
jams100 <43343134+jams100@users.noreply.github.com>
2020-04-08 19:09:47 +0100

    Coding standards and Example java files demonstrating coding standards

---
+++ namingConventions/IndentationExample.java
@@ -0,0 +1,13 @@
+package namingConventions;
+
+/**
+ * @author C15339336
+ * This class shows our indentation style
+ */
+public class IndentationExample {
+    //Indented 4 spaces in
+    public IndentationExample() {
+	    // TODO Auto-generated constructor stub 
+	}
+
+}
